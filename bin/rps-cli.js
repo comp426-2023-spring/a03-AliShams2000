5865323671cad9225f9f9aad44dc647010f10c57
Ashams2000 <alishamsol2000@gmail.com>
2023-04-13 21:12:28 -0400

    Completed rps-cli.js file

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -1,34 +1,47 @@
 #!/usr/bin/env node
+
 import { rps } from '../lib/rpsls.js'
 import minimist from "minimist"
 
 const args = minimist(process.argv.slice(2));
 
-// displays the help message if -h or -help is given in command prompt and exits application
-if (args.h || args.help){
-	console.log('Usage: node-rps [SHOT]\nPlay Rock Paper Scissors (RPS)\n\n',
-    '  -h, --help      display this help message and exit\n',
-    '  -r, --rules     display the rules and exit\n\nExamples:\n',
-    '  node-rps        Return JSON with single player RPS result.\n',
-    '                  e.g. {"player":"rock"}\n',
-    '  node-rps rock   Return JSON with results for RPS played against a simulated opponent.\n',
-    '                  e.g {"player":"rock","opponent":"scissors","result":"win"}')
+if(args.help_message || args.h){
+	help_message();
+	process.exit(0);
+}
+if(args.rules_message || args.r){
+	rules_message();
 	process.exit(0);
 }
 
-// displays the rules if -r or -rules is given in command prompt and exits application
-if (args.r || args.rules){
-	onsole.log('Rules for Rock Paper Scissors:\n\n',
-    ' - Scissors CUTS Paper\n',
-    ' - Paper COVERS Rock\n',
-    ' - Rock CRUSHES Scissors')
+const move_choice = args._[0]
+try{
+	console.log(JSON.stringify(rps(move_choice)));
+} catch (e){
+	help_message();
+	rules_message();
 	process.exit(0);
 }
 
-if (args._.length <= 2) {
-    console.log(JSON.stringify(rps()))
-} else if (args._.length > 3) {
-    console.error("ERROR: arguments out of range")
-} else {
-    console.log(JSON.stringify(rps(...args._.slice(2))))
+
+// Displays the help message if -h or -help is given in command prompt and exits application
+function help_message(){
+	console.log(`Usage: node-rps [SHOT]
+	Play Rock Paper Scissors (RPS)
+ 		 -h, --help      display this help message and exit
+ 		 -r, --rules     display the rules and exit
+	Examples:
+  		node-rps        Return JSON with single player RPS result.
+                  		e.g. {"player":"rock"}
+  		node-rps rock   Return JSON with results for RPS played against a simulated opponent.
+                  		e.g {"player":"rock","opponent":"scissors","result":"win"}`);
+}
+
+
+// Displays the rules if -r or -rules is given in command prompt and exits application
+function rules_message(){
+	console.log(`Rules for Rock Paper Scissors:
+			- Scissors CUTS Paper
+			- Paper COVERS Rock
+			- Rock CRUSHES Scissors`);
 }
